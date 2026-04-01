276ac09b977defe96159bb7c94d29df292638116
Linus Tran <linh@Linuss-MacBook-Pro.local>
2026-04-01 21:02:22 +0700

    fix: remove fqa padding left

--- src/modules/Home/components/fqa/index.tsx
+++ src/modules/Home/components/fqa/index.tsx
@@ -7,7 +7,7 @@ export const FQA = async () => {
   return (
     <div className='bg-[#F9F9F9] px-6'>
       <div className='mx-auto max-w-[1280px] pt-24 pb-24'>
-        <div className='mb-10 pl-6 max-md:pl-3'>
+        <div className='mb-10 max-md:pl-3'>
           <CommonTitle as='h2' className='text-center max-lg:text-3xl'>
             FQA
           </CommonTitle>
