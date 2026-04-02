e7d0bc7336e011eb18c7182bca46c06ff93a272a
Linus Tran <linh@Linuss-MacBook-Pro.local>
2026-04-02 11:46:37 +0700

    fix: header nav

--- src/components/layouts/MainLayout/components/Header/components/header-links.tsx
+++ src/components/layouts/MainLayout/components/Header/components/header-links.tsx
@@ -1,24 +1,31 @@
 'use client';
 
 import { handleScroll } from '@/lib/utils';
+import { ROUTES } from '@/utils/routes';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { NAV_LINKS } from '..';
 
 export const HeaderLinks = () => {
+  const pathname = usePathname();
+
   return (
     <ul className='flex items-center gap-5 font-medium text-base max-xl:hidden'>
       {NAV_LINKS.map((link) => (
         <li key={link.label}>
-          <a
-            href={`#${link.elId}`}
+          <Link
+            href={`${ROUTES.HOME}#${link.elId}`}
             onClick={(e) => {
-              e.preventDefault();
-              handleScroll(link.elId);
+              if (pathname === ROUTES.HOME) {
+                e.preventDefault();
+                handleScroll(link.elId);
+              }
             }}
             className='cursor-pointer'
           >
             {link.label}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
